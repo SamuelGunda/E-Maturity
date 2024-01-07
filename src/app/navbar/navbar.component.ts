6b75effa85668a3f7cac1cae5850ef8b353d3571
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-01-07 13:35:51 +0100

    styling changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import { SharedService } from '../shared.service';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(private sharedService: SharedService) {}
 
-  get isDisplayed() {
-    return this.sharedService.getVisibility();
-  }
 }
