c8d556b20a1d07959b29f705037f39adafabcd97
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-01-15 13:29:19 +0100

    designed login card -> styling work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,6 @@ export class AppComponent {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
-  toggleNav(): void {
-    const navbar = document.getElementById('navbar') as HTMLElement;
-    const landingPage = document.getElementById('landingPage') as HTMLElement;
-
-    if (navbar.style.display === 'none') {
-      navbar.style.display = 'block';
-      navbar.style.display = '0 1 20%';
-      landingPage.style.flex = '1 1 75%';
-    } else {
-      navbar.style.display = 'none';
-      landingPage.style.flexBasis = '100%';
-    }
-  }
-
   headerfixes: boolean = false;
 
   @HostListener('window:scroll', []) onScroll(){
