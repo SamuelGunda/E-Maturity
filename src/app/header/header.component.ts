71ebd2d6a28fb896a48d58cb4830d7d02ab3e46a
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-01-17 11:28:32 +0100

    additionla styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,13 +7,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+buttonExpanded: any = false;
+
   onToggleNav(): void {
     const navbar = document.getElementById('navbar') as HTMLElement;
 
     if (navbar.style.display === 'block') {
       navbar.style.display = 'none';
+      this.buttonExpanded = !this.buttonExpanded;
     } else {
       navbar.style.display = 'block';
+      this.buttonExpanded = !this.buttonExpanded;
     }
   }
 }
