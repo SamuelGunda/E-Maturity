589b78a0aad08335b5f3e9f393643cc9dabbc39c
Samuel Gunda <samuel.gunda@kosickaakademia.sk>
2024-01-21 17:16:14 +0100

    Updated authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,28 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth} from '@angular/fire/compat/auth';
-
-
+import {Observable} from "rxjs";
+import firebase from "firebase/compat";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+isLoggedIn = false
+userData:Observable<firebase.User>
 
-
-  constructor(private afAuth: AngularFireAuth) { }
-  
+  constructor(private afAuth: AngularFireAuth) {
+  // @ts-ignore
+  this.userData = afAuth.authState;
+  }
 
   register(email: string, password: string) {
     return this.afAuth.createUserWithEmailAndPassword(email, password);
   }
 
   login(email: string, password: string) {
-    return this.afAuth.signInWithEmailAndPassword(email, password);
+     return this.afAuth.signInWithEmailAndPassword(email, password)
+      .then(res=>{
+      this.isLoggedIn = true;
+      localStorage.setItem("email", JSON.stringify(res.user))
+    })
   }
 
   logout() {
-    return this.afAuth.signOut();
+    this.afAuth.signOut()
+    localStorage.removeItem("email");
   }
-
-  
 }
