d0772c71a2ae816568f8659bbb51726599a8a4fe
Samuel Gunda <samuel.gunda.sg@gmail.com>
2024-01-28 22:44:43 +0100

    Auth fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponent } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { environment } from './environments/environment';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NgOptimizedImage } from "@angular/common";
+import { NgOptimizedImage } from '@angular/common';
 import { YearsPageComponent } from './years-page/years-page.component';
 
-import {HttpClientModule} from "@angular/common/http";
-import {provideFirebaseApp} from "@angular/fire/app";
-import firebase from "firebase/compat/app";
+import { HttpClientModule } from '@angular/common/http';
+import { provideFirebaseApp } from '@angular/fire/app';
+import firebase from 'firebase/compat/app';
 import initializeApp = firebase.initializeApp;
-import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { TestPageComponent } from './test-page/test-page.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { UserAccountComponent } from './user-account/user-account.component';
@@ -38,15 +40,14 @@ import { UserAccountComponent } from './user-account/user-account.component';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     // FirestoreModule,
-    // AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(environment.firebase),
     // AngularFirestoreModule,
-    // AngularFireAuthModule,
+    AngularFireAuthModule,
     NgOptimizedImage,
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
