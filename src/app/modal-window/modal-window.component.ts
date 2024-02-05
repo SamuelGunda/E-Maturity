76a16f9ecee63a082fb7eb2a2af440e028a9d285
Patrik Lišivka <patrik.lisivka@kosickaakademia.sk>
2024-02-05 14:02:36 +0100

    added dark mode to modal window

--- src/app/modal-window/modal-window.component.ts
+++ src/app/modal-window/modal-window.component.ts
@@ -1,25 +1,33 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ModalData } from '../test-page/test-page.component';
 import { TestService } from '../service/test-service/test.service';
 import { AuthService } from '../auth.service';
+import { DarkModeService } from '../dark-mode.service';
 
 @Component({
   selector: 'app-modal-window',
   templateUrl: './modal-window.component.html',
   styleUrls: ['./modal-window.component.css'],
 })
-export class ModalWindowComponent {
+export class ModalWindowComponent implements OnInit{
   authService: AuthService;
+  isDarkMode: boolean = false;
+
   constructor(
     public dialogRef: MatDialogRef<ModalWindowComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ModalData,
     private testService: TestService,
     authService: AuthService,
+    private darkModeService: DarkModeService
   ) {
     this.authService = authService;
   }
-
+  ngOnInit() {
+    this.darkModeService.isDarkMode$.subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
+    });
+  }
   onNoClick(): void {
     this.dialogRef.close();
   }
