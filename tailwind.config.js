8398da65bca62c58e4720daa89466714ee6a0663
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-03-09 18:25:35 +0100

    updated landing-page to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        first_landing_page_card : "url('/assets/card1_wave_coloured.jpg')",
+        second_landing_page_card : "url('/assets/card2_wave_coloured.jpg')",
+        third_landing_page_card : "url('/assets/card3_wave_coloured.jpg')",
+        fourth_landing_page_card : "url('/assets/card4_wave_coloured.jpg')",
+      },
       colors:{ 
         ang: {
           main_bg : '#FBFEF9',
-          text_main : '',
+          text_main : '#3369f3',
           text_white : '#FBFEF9',
           border_main : '#3369f3',
           main_btn : '#3369f3',
