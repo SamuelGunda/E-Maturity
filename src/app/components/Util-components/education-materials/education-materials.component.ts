929d96d5fe8706f195b90c042c77c8ccb1f34dee
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-03-10 13:23:03 +0100

    educational materials to tailwind

--- src/app/components/Util-components/education-materials/education-materials.component.ts
+++ src/app/components/Util-components/education-materials/education-materials.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
-import { DarkModeService } from '../../../service/dark-mode-serivce/dark-mode.service';
+import { Component, OnInit } from '@angular/core';
 import {
   EduService,
   eduMaterials,
@@ -13,83 +12,33 @@ import {
 export class EducationMaterialsComponent implements OnInit {
   sjlMaterials: eduMaterials[] = [];
   anjMaterials: eduMaterials[] = [];
+  matMaterials: eduMaterials[] = [];
 
   showSjlNotes: boolean = false;
   showAnjNotes: boolean = false;
   showMatNotes: boolean = false;
   isDarkMode: boolean = false;
 
-  constructor(
-    private darkModeService: DarkModeService,
-    private eduService: EduService,
-    private renderer: Renderer2,
-    private el: ElementRef,
-  ) {}
+  constructor(private eduService: EduService) {}
 
   ngOnInit() {
-    this.darkModeService.isDarkMode$.subscribe((isDarkMode) => {
-      this.isDarkMode = isDarkMode;
-    });
     this.eduService.getFile('SJL').subscribe((materials) => {
       this.sjlMaterials = materials;
     });
     this.eduService.getFile('ANJ').subscribe((materials) => {
       this.anjMaterials = materials;
     });
+    this.eduService.getFile('MAT').subscribe((materials) => {
+      this.matMaterials = materials;
+    });
   }
   toggleShowSjlNotes() {
     this.showSjlNotes = !this.showSjlNotes;
-    if (this.showSjlNotes) {
-      this.renderer.setStyle(
-        this.el.nativeElement.querySelector('.main_container'),
-        'height',
-        '119rem',
-      );
-    } else if (this.showAnjNotes) {
-      this.renderer.setStyle(
-        this.el.nativeElement.querySelector('.main_container'),
-        'height',
-        '92rem',
-      );
-    } else {
-      this.renderer.setStyle(
-        this.el.nativeElement.querySelector('.main_container'),
-        'height',
-        '100%',
-      );
-    }
   }
   toggleShowAnjNotes() {
     this.showAnjNotes = !this.showAnjNotes;
-    if (this.showAnjNotes) {
-      if (this.showSjlNotes) {
-        this.renderer.setStyle(
-          this.el.nativeElement.querySelector('.main_container'),
-          'height',
-          '119rem',
-        );
-      }
-    }
-    if (this.showAnjNotes) {
-      if (!this.showSjlNotes) {
-        this.renderer.setStyle(
-          this.el.nativeElement.querySelector('.main_container'),
-          'height',
-          '92rem',
-        );
-      }
-    } else if (this.showSjlNotes) {
-      this.renderer.setStyle(
-        this.el.nativeElement.querySelector('.main_container'),
-        'height',
-        '119rem',
-      );
-    } else {
-      this.renderer.setStyle(
-        this.el.nativeElement.querySelector('.main_container'),
-        'height',
-        '100%',
-      );
-    }
+  }
+  toggleShowMatNotes() {
+    this.showMatNotes = !this.showMatNotes;
   }
 }
