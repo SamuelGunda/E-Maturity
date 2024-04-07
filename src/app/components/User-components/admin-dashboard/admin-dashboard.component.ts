1df87b3d729960d0fdba95a0b61c1d8c8078e8ee
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-04-07 15:36:02 +0200

    displays teacher on dashboard from school, for admin page

--- src/app/components/User-components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/User-components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Teacher } from 'src/app/model/teacher';
+import { DashboardService } from 'src/app/service/dashboard-service/dashboard.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component.css'],
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  teacherInfo$: Observable<Teacher[]> | undefined;
+  schoolName: string | undefined;
 
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.teacherInfo$ = this.dashboardService.getTeacherInfo();
+    this.setSchoolName();
+  }
+
+  setSchoolName(): void {
+    this.teacherInfo$?.subscribe((teachers) => {
+      if (teachers.length > 0) {
+        this.schoolName = teachers[0].schoolName;
+      }
+    });
+  }
 }
