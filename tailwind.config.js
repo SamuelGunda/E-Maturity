3f8e50abfa3560ee1a2aa1e7ab08f71717cf1d6f
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-05-01 13:05:36 +0200

    styling changes, also modified dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.ts"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      backgroundImage: {
-        first_landing_page_card: "url('/assets/card1_wave_coloured.jpg')",
-        second_landing_page_card: "url('/assets/card2_wave_coloured.jpg')",
-        third_landing_page_card: "url('/assets/card3_wave_coloured.jpg')",
-        fourth_landing_page_card: "url('/assets/card4_wave_coloured.jpg')",
-      },
-      colors: {
-        zinc: {
-          850: "#363638",
+    content: ["./src/**/*.html", "./src/**/*.ts"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            backgroundImage: {
+                first_landing_page_card:
+                    "url('/assets/card1_wave_coloured.jpg')",
+                second_landing_page_card:
+                    "url('/assets/card2_wave_coloured.jpg')",
+                third_landing_page_card:
+                    "url('/assets/card3_wave_coloured.jpg')",
+                fourth_landing_page_card:
+                    "url('/assets/card4_wave_coloured.jpg')",
+            },
+            colors: {
+                ang: {
+                    gradient_from:
+                        "hsl(var(--color-gradient_from) / <alpha-value>)",
+
+                    gradient_to:
+                        "hsl(var(--color-gradient_to) / <alpha-value>)",
+
+                    main_bg: "hsl(var(--color-main_bg) / <alpha-value>)",
+
+                    text_main: "hsl(var(--color-text_main) / <alpha-value>)",
+
+                    text_white: "hsl(var(--color-text_white) / <alpha-value>)",
+
+                    border_main:
+                        "hsl(var(--color-border_main) / <alpha-value>)",
+
+                    main_btn: "hsl(var(--color-main_btn) / <alpha-value>)",
+                },
+            },
+            boxShadow: {
+                inner_sh: "inset 0 0 12px gray",
+                dark_sh: "inset 0 0 6px #71717a",
+            },
         },
-        ang: {
-          gradient_from: "#06b7f9",
-          gradient_to: "#194aec",
-          main_bg: "#FBFEF9",
-          text_main: "#3369f3",
-          text_white: "#FBFEF9",
-          border_main: "#3369f3",
-          main_btn: "#3369f3",
-        },
-      },
-      boxShadow: {
-        inner_sh: "inset 0 0 12px gray",
-        dark_sh: "inset 0 0 6px #71717a",
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
