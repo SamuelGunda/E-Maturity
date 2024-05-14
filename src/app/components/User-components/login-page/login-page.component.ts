3b7aeeb9b03c256e5e11c7954d3845f7ae867149
Rastislav Pacut <rastislav.pacut@kosickaakademia.sk>
2024-05-14 11:15:12 +0200

    minor fixes

--- src/app/components/User-components/login-page/login-page.component.ts
+++ src/app/components/User-components/login-page/login-page.component.ts
@@ -3,52 +3,52 @@ import { AuthService } from '../../../service/auth-serivce/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css'],
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
-  submitted = false;
-  name: string = '';
-  password: string = '';
-  error: boolean = false;
-  rememberMe: boolean = false;
-  isDarkMode: boolean = false;
-  loggedIn: boolean = false;
+    submitted = false;
+    name: string = '';
+    password: string = '';
+    error: boolean = false;
+    rememberMe: boolean = false;
+    isDarkMode: boolean = false;
+    loggedIn: boolean = false;
 
-  constructor(
-    private router: Router,
-    private service: AuthService,
-  ) {}
+    constructor(
+        private router: Router,
+        private service: AuthService,
+    ) {}
 
-  submit() {
-    this.submitted = true;
-  }
-
-  newInput() {
-    this.error = false;
-  }
-
-  login() {
-    this.service
-      .login(this.name, this.password, this.rememberMe)
-      .then(() => this.router.navigate(['']))
-      .catch(() => (this.error = true));
-  }
-
-  signInWithGoogle() {
-    this.service
-      .googleSignIn()
-      .then(() => this.router.navigate(['']))
-      .catch(() => (this.error = true));
-  }
-  ngOnInit() {
-    const savedUser = localStorage.getItem('rememberMe');
-    if (savedUser) {
-      const user = JSON.parse(savedUser);
-      console.log(user);
-      this.name = user.email;
-      this.password = user.password;
+    submit() {
+        this.submitted = true;
     }
-  }
+
+    newInput() {
+        this.error = false;
+    }
+
+    login() {
+        this.service
+            .login(this.name, this.password, this.rememberMe)
+            .then(() => this.router.navigate(['']))
+            .catch(() => (this.error = true));
+    }
+
+    signInWithGoogle() {
+        this.service
+            .googleSignIn()
+            .then(() => this.router.navigate(['']))
+            .catch(() => (this.error = true));
+    }
+    ngOnInit() {
+        const savedUser = localStorage.getItem('rememberMe');
+        if (savedUser) {
+            const user = JSON.parse(savedUser);
+            console.log(user);
+            this.name = user.email;
+            this.password = user.password;
+        }
+    }
 }
