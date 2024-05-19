f75f588b7f5cdfe07ba9dfa7aca2584749e856d0
Patrik Lišivka <patrik.lisivka@kosickaakademia.sk>
2024-05-19 12:33:08 +0200

    added user-statistic model

---
+++ src/app/model/user-statistics.ts
@@ -0,0 +1,5 @@
+export interface UserStatistic {
+    subCat: string; // The subcategory of the test.
+    percentageScore: number; // The percentage score of the test.
+    timeTaken: string; // The time taken to complete the test.
+}
