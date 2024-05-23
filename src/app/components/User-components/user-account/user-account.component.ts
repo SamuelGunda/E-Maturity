552ba15ffa008072dd7cd958313bd05241043057
Patrik Lišivka <patrik.lisivka@kosickaakademia.sk>
2024-05-23 12:04:12 +0200

    added user statistic methods and service

--- src/app/components/User-components/user-account/user-account.component.ts
+++ src/app/components/User-components/user-account/user-account.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../service/auth-serivce/auth.service';
 import { UserAccountService } from '../../../service/user-acc-service/user-acc.service';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { CookieService } from 'ngx-cookie';
+import { UserStatistic } from 'src/app/model/user-statistics';
 
 @Component({
   selector: 'app-user-account',
@@ -12,16 +14,50 @@ export class UserAccountComponent implements OnInit {
   userName: string = '';
   isDarkMode: boolean = false;
   darkMode: boolean = false;
+  sjlStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
+  anjStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
+  matStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
+  sjlTestCount: number = 0;
+  anjTestCount: number = 0;
+  matTestCount: number = 0;
+  sjlAveragePercentage: number = 0;
+  anjAveragePercentage: number = 0;
+  matAveragePercentage: number = 0;
+  sjlAverageTime: string = '00:00:00';
+  anjAverageTime: string = '00:00:00';
+  matAverageTime: string = '00:00:00';
 
   constructor(
     private authService: AuthService,
     public userAccountService: UserAccountService,
-  ) {}
+    private cookieService: CookieService,
+  ) { }
 
   ngOnInit() {
     this.authService.userData.subscribe((user) => {
       if (user) {
         this.userName = user.displayName || '';
+        const uid = this.cookieService.get('uid');
+        if (uid) {
+          this.userAccountService.getTestCountBySubject(uid, 'sjl').subscribe(count => this.sjlTestCount = count);
+          this.userAccountService.getTestCountBySubject(uid, 'anj').subscribe(count => this.anjTestCount = count);
+          this.userAccountService.getTestCountBySubject(uid, 'mat').subscribe(count => this.matTestCount = count);
+          this.sjlStatistics$ = this.userAccountService.getTestStatistics(uid, "SJL");
+          this.anjStatistics$ = this.userAccountService.getTestStatistics(uid, "ANJ");
+          this.matStatistics$ = this.userAccountService.getTestStatistics(uid, "MAT");
+          this.sjlStatistics$.subscribe(stats => {
+            this.sjlAveragePercentage = stats.averagePercentage;
+            this.sjlAverageTime = stats.averageTime;
+          });
+          this.anjStatistics$.subscribe(stats => {
+            this.anjAveragePercentage = stats.averagePercentage;
+            this.anjAverageTime = stats.averageTime;
+          });
+          this.matStatistics$.subscribe(stats => {
+            this.matAveragePercentage = stats.averagePercentage;
+            this.matAverageTime = stats.averageTime;
+          });        
+        }
       }
     });
   }
--- src/app/service/user-acc-service/user-acc.service.ts
+++ src/app/service/user-acc-service/user-acc.service.ts
@@ -1,23 +1,91 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, from, of } from 'rxjs';
+import { BehaviorSubject, Observable, forkJoin, from, map, of, switchMap } from 'rxjs';
 import {
   AngularFirestore,
   DocumentReference,
 } from '@angular/fire/compat/firestore';
 import * as XLSX from 'xlsx';
-
+import { CollectionReference, DocumentData, Firestore, QuerySnapshot, collection, doc, getDocs } from 'firebase/firestore';
+import { UserStatistic } from 'src/app/model/user-statistics';
 @Injectable({
   providedIn: 'root',
 })
 export class UserAccountService {
-
-  private localStorageKey = 'savedQuestions';
 
   constructor(
     private firestore: AngularFirestore,
   ) {}
 
   selectedIcon!: string;
+
+  getTestStatistics(uid: string, subCat: string): Observable<{ averagePercentage: number, averageTime: string }> {
+    const dataCollection = this.firestore.collection('users');
+    const documentRef = dataCollection.doc(uid);
+    const userStatisticsCollectionRef = documentRef.collection('userStatistics');
+    const subjectDocumentRef = userStatisticsCollectionRef.doc('subjects');
+    const subjectCollectionRef = subjectDocumentRef.collection(subCat);
+
+    return from(subjectCollectionRef.get()).pipe(
+      map((querySnapshot) => {
+        const statistics: UserStatistic[] = [];
+
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const userStatistic: UserStatistic = {
+            percentageScore: data['percentageScore'],
+            timeTaken: data['timeTaken']
+          };
+          statistics.push(userStatistic);
+        });
+
+        // Calculate average percentage and time
+        const averagePercentage = this.calculateAveragePercentage(statistics);
+        const averageTime = this.calculateAverageTime(statistics);
+
+        return {
+          averagePercentage: averagePercentage,
+          averageTime: averageTime
+        };
+      })
+    );
+  }
+
+  private calculateAveragePercentage(statistics: UserStatistic[]): number {
+    if (statistics.length === 0) return 0;
+    const totalPercentage = statistics.reduce((sum, stat) => sum + stat.percentageScore, 0);
+    return totalPercentage / statistics.length;
+  }
+
+  private calculateAverageTime(statistics: UserStatistic[]): string {
+    if (statistics.length === 0) return '00:00:00';
+    const totalSeconds = statistics.reduce((sum, stat) => sum + this.timeStringToSeconds(stat.timeTaken), 0);
+    const averageSeconds = totalSeconds / statistics.length;
+    return this.secondsToTimeString(averageSeconds);
+  }
+
+  private timeStringToSeconds(time: string): number {
+    const parts = time.split(':');
+    return (+parts[0]) * 3600 + (+parts[1]) * 60 + (+parts[2]);
+  }
+
+  private secondsToTimeString(seconds: number): string {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
+    return [h, m, s].map(v => v < 10 ? '0' + v : v).join(':');
+  }
+
+  getTestCountBySubject(uid: string, subCat: string): Observable<number> {
+    const userStatisticsCollectionRef = this.firestore.collection('users').doc(uid).collection('userStatistics');
+    const subjectCollectionRef = userStatisticsCollectionRef.doc('subjects').collection(subCat);
+
+    return from(subjectCollectionRef.get()).pipe(
+      map((querySnapshot) => {
+        return querySnapshot.size; // Vráti počet dokumentov v kolekcii
+      })
+    );
+  }
+
 
   getAvailableIcons(): string[] {
     return [
