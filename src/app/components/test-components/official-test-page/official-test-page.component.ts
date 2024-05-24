55fb5166a5d56c1d6fa141651e10b6ee4edb543f
Rastislav Pačut <rastislav.pacut@kosickaakademia.sk>
2024-05-24 09:36:39 +0000

    Revert "working test constructor"
    This reverts commit d6557382ec384fea3003d113bd2ac297383a61aa

--- src/app/components/test-components/official-test-page/official-test-page.component.ts
+++ src/app/components/test-components/official-test-page/official-test-page.component.ts
@@ -116,8 +116,6 @@ export class OfficialTestPageComponent {
                                 ', ' +
                                 question.userAnswer_2.trim();
                         }
-                    } else if (question.questionType === 'canceled') {
-                        question.userAnswer = '*';
                     }
 
                     if (
@@ -142,12 +140,8 @@ export class OfficialTestPageComponent {
             }
             this.testService.getTestResult(testResults).subscribe(
                 (result: TestResult) => {
-                    (this.testResults = result),
-                        this.testService.saveTestStatistics(
-                            testResults.subCat,
-                            testResults.percentageScore,
-                            testResults.timeTaken,
-                        );
+                    this.testResults = result,
+                    this.testService.saveTestStatistics(testResults.subCat, testResults.percentageScore, testResults.timeTaken);
                 },
                 (error) => {
                     console.error('Error updating test results:', error);
