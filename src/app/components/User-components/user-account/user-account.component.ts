9930ac3f3b555ff1e3d0ee1aa86eb86f51f57515
Patrik Lišivka <patrik.lisivka@kosickaakademia.sk>
2024-05-24 11:14:33 +0200

    merge conflicts solved

--- src/app/components/User-components/user-account/user-account.component.ts
+++ src/app/components/User-components/user-account/user-account.component.ts
@@ -14,77 +14,56 @@ export class UserAccountComponent implements OnInit {
     userName: string = '';
     isDarkMode: boolean = false;
     darkMode: boolean = false;
-    sjlStatistics$:
-        | Observable<{ averagePercentage: number; averageTime: string }>
-        | undefined;
-    anjStatistics$:
-        | Observable<{ averagePercentage: number; averageTime: string }>
-        | undefined;
-    matStatistics$:
-        | Observable<{ averagePercentage: number; averageTime: string }>
-        | undefined;
+    sjlStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
+    anjStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
+    matStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
+
     sjlTestCount: number = 0;
     anjTestCount: number = 0;
     matTestCount: number = 0;
+
     sjlAveragePercentage: number = 0;
     anjAveragePercentage: number = 0;
     matAveragePercentage: number = 0;
+
     sjlAverageTime: string = '00:00:00';
     anjAverageTime: string = '00:00:00';
     matAverageTime: string = '00:00:00';
-  userName: string = '';
-  isDarkMode: boolean = false;
-  darkMode: boolean = false;
-  sjlStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
-  anjStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
-  matStatistics$: Observable<{ averagePercentage: number; averageTime: string; }> | undefined;
-
-  sjlTestCount: number = 0;
-  anjTestCount: number = 0;
-  matTestCount: number = 0;
-
-  sjlAveragePercentage: number = 0;
-  anjAveragePercentage: number = 0;
-  matAveragePercentage: number = 0;
-
-  sjlAverageTime: string = '00:00:00';
-  anjAverageTime: string = '00:00:00';
-  matAverageTime: string = '00:00:00';
 
     constructor(
         private authService: AuthService,
         public userAccountService: UserAccountService,
         private cookieService: CookieService,
-    ) {}
+    ) { }
 
-  ngOnInit() {
-    this.authService.userData.subscribe((user) => {
-      if (user) {
-        this.userName = user.displayName || '';
-        const uid = this.cookieService.get('uid');
-        if (uid) {
-          this.userAccountService.getTestCountBySubject(uid, 'sjl').subscribe(count => this.sjlTestCount = count);
-          this.userAccountService.getTestCountBySubject(uid, 'anj').subscribe(count => this.anjTestCount = count);
-          this.userAccountService.getTestCountBySubject(uid, 'mat').subscribe(count => this.matTestCount = count);
+    ngOnInit() {
+        this.authService.userData.subscribe((user) => {
+            if (user) {
+                this.userName = user.displayName || '';
+                const uid = this.cookieService.get('uid');
+                if (uid) {
+                    this.userAccountService.getTestCountBySubject(uid, 'sjl').subscribe(count => this.sjlTestCount = count);
+                    this.userAccountService.getTestCountBySubject(uid, 'anj').subscribe(count => this.anjTestCount = count);
+                    this.userAccountService.getTestCountBySubject(uid, 'mat').subscribe(count => this.matTestCount = count);
 
-          this.userAccountService.getTestStatistics(uid, 'SJL').subscribe(stats => {
-            this.sjlAveragePercentage = stats.averagePercentage;
-            this.sjlAverageTime = stats.averageTime;
-          });
+                    this.userAccountService.getTestStatistics(uid, 'SJL').subscribe(stats => {
+                        this.sjlAveragePercentage = stats.averagePercentage;
+                        this.sjlAverageTime = stats.averageTime;
+                    });
 
-          this.userAccountService.getTestStatistics(uid, 'ANJ').subscribe(stats => {
-            this.anjAveragePercentage = stats.averagePercentage;
-            this.anjAverageTime = stats.averageTime;
-          });
+                    this.userAccountService.getTestStatistics(uid, 'ANJ').subscribe(stats => {
+                        this.anjAveragePercentage = stats.averagePercentage;
+                        this.anjAverageTime = stats.averageTime;
+                    });
 
-          this.userAccountService.getTestStatistics(uid, 'MAT').subscribe(stats => {
-            this.matAveragePercentage = stats.averagePercentage;
-            this.matAverageTime = stats.averageTime;
-          });
-        }
-      }
-    });
-  }
+                    this.userAccountService.getTestStatistics(uid, 'MAT').subscribe(stats => {
+                        this.matAveragePercentage = stats.averagePercentage;
+                        this.matAverageTime = stats.averageTime;
+                    });
+                }
+            }
+        });
+    }
 
     darkToggle = document.querySelector('.toggle_dark');
 
